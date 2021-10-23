1a4bb1dad95d1aaf78865446ea893fb88da76499
Gonzalo Bertinat <gonzalo_bertinat_98@hotmail.com>
2021-10-22 21:25:32 -0300

    Desafío 5 - Funciones relacionadas

--- js/main.js
+++ js/main.js
@@ -7,14 +7,18 @@
 
 /* Cotizaciones en USD de las principales criptomonedas.
    A futuro, se pedirán vía API */
-const BITCOIN = 60000
-const ETHEREUM = 3600
-const CARDANO = 2.20
+const PRECIO_BITCOIN = 60000
+const PRECIO_ETHEREUM = 3600
+const PRECIO_CARDANO = 2.20
 
 // Precio DOLAR en Pesos Argentinos
 const COTIZACION_USD = 185
 
-const convertirAPesos = (dolares) => (dolares * COTIZACION_USD).toFixed(2)
+const formatoMoneda = cantidad => cantidad.toFixed(2)
+
+const formatoCripto = cantidad => cantidad.toFixed(6)
+
+const convertirAPesos = dolares => formatoMoneda(dolares * COTIZACION_USD)
 
 const calcularSaldo = (precio, unidades) => precio * unidades
 
@@ -27,20 +31,36 @@ const imprimirSaldos = () => {
     document.write(`<h3>SALDOS BILLETERA:</h3>
                     <div class="saldos">
                         <span class="pesos">Pesos (ARS): $${convertirAPesos(saldoUSD)}</span>
-                        <span class="dolares">Dólares (USD): $${saldoUSD.toFixed(2)}</span>
+                        <span class="dolares">Dólares (USD): $${formatoMoneda(saldoUSD)}</span>
                     </div>`)
 }
 
-const registrarTransaccion = (moneda, precio) => {
-    let unidades = prompt(`Ingrese el monto en ${moneda}:`)
-    while(isNaN(unidades) || unidades <= 0){
-        unidades = prompt(`El valor ingresado es incorrecto.\nPor favor, ingrese el monto en ${moneda}:`)
+const ingresarMonto = moneda => {
+    let monto = prompt(`Ingrese el monto en ${moneda}:`)
+    while(isNaN(monto) || monto <= 0){
+        monto = prompt(`El valor ingresado es incorrecto.\nPor favor, ingrese el monto en ${moneda}:`)
     }
+    return parseFloat(monto)
+}
+
+const imprimirTransaccion = (moneda, unidades, valorUSD) => {
     document.write(`<div class="transaccion__container">
-                         <span class="transaccion">+ ${parseFloat(unidades).toFixed(6)} ${moneda}</span>
+                         <span class="transaccion">+ ${formatoCripto(unidades)} ${moneda} = ${formatoMoneda(valorUSD)} USD</span>
                     </div>`)
     transacciones++;
-    return calcularSaldo(precio,unidades)
+}
+
+/* DESAFIO COMPLEMENTARIO
+* El registro de una transacción se compone de 3 pasos.
+* 1. Ingresar el monto en unidades de la moneda elegida. 
+* 2. Calcular su valor monetario en USD.
+* 3. Mostrarla al usuario (imprimirla en el HTML).
+*/ 
+const registrarTransaccion = (moneda, precio) => {
+    let monto = ingresarMonto(moneda)
+    let valorUSD = calcularSaldo(precio, monto)
+    imprimirTransaccion(moneda, monto, valorUSD)
+    return valorUSD
 }
 
 const cargarTransacciones = () => {
@@ -50,13 +70,13 @@ const cargarTransacciones = () => {
     while(opcion !== "4"){
         switch(opcion){
             case "1":
-                saldoUSD += registrarTransaccion("BTC", BITCOIN)
+                saldoUSD += registrarTransaccion("BTC", PRECIO_BITCOIN)
                 break
             case "2":
-                saldoUSD += registrarTransaccion("ETH", ETHEREUM)
+                saldoUSD += registrarTransaccion("ETH", PRECIO_ETHEREUM)
                 break
             case "3":
-                saldoUSD += registrarTransaccion("ADA", CARDANO)
+                saldoUSD += registrarTransaccion("ADA", PRECIO_CARDANO)
                 break
             default:
                 alert("ERROR: Opción no encontrada.")

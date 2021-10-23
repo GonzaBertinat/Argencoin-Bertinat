59264789dee165e9bc13980eeec97a391e5d598c
Gonzalo Bertinat <gonzalo_bertinat_98@hotmail.com>
2021-10-22 21:26:30 -0300

    Desafío 6 - Incorporar objetos

--- js/main.js
+++ js/main.js
@@ -1,29 +1,66 @@
-/* Desafío 4
+/* Desafío Objetos
  * El proyecto consistirá en una plataforma de criptomonedas.
  * Contará con la posibilidad de ver cotizaciones en vivo, cargar transacciones, 
- * y ver el balance de activos. 
- * Para este desafío simularé la carga de activos del usuario.
+ * y ver el balance de activos. También el usuario podrá registrarse e iniciar sesión.
+ * 
+ * Para este desafío incluyo la entidad Transaccion, la entidad Criptomoneda, las relaciono entre ellas
+ * y adapto la funcionalidad existente de carga de transacciones y vista de balances a estas clases. 
  */
 
-/* Cotizaciones en USD de las principales criptomonedas.
-   A futuro, se pedirán vía API */
-const PRECIO_BITCOIN = 60000
-const PRECIO_ETHEREUM = 3600
-const PRECIO_CARDANO = 2.20
+// Entidades del sistema.
+class Criptomoneda {
+    constructor(sigla, nombre, cotizacion){
+        this.sigla = sigla
+        this.nombre = nombre
+        this.cotizacion = cotizacion
+    }
 
-// Precio DOLAR en Pesos Argentinos
+    nombreFormateado(){
+        return `${this.sigla} - ${this.nombre}` 
+    }
+}
+
+class Transaccion {
+    constructor(criptomoneda, unidades){
+        this.criptomoneda = criptomoneda
+        this.unidades = unidades
+    }
+
+    montoEnUSD(){
+        return this.criptomoneda.cotizacion * this.unidades
+    }
+}
+
+/* Criptomonedas disponibles. 
+ * Las cotizaciones son en USD. 
+ * A futuro se obtendrán vía API, al igual que la cotización del dólar en pesos argentinos.
+ */
+const bitcoin = new Criptomoneda('BTC','Bitcoin', 63000)
+const ethereum = new Criptomoneda('ETH', 'Ethereum', 3800)
+const cardano = new Criptomoneda('ADA','Cardano', 2.10)
+   
+// Precio DÓLAR en Pesos Argentinos.
 const COTIZACION_USD = 185
 
+/**  Funciones utilitarias y de conversión. */
 const formatoMoneda = cantidad => cantidad.toFixed(2)
-
-const formatoCripto = cantidad => cantidad.toFixed(6)
-
+const formatoCripto = cantidad => cantidad.toFixed(8)
 const convertirAPesos = dolares => formatoMoneda(dolares * COTIZACION_USD)
 
-const calcularSaldo = (precio, unidades) => precio * unidades
+// Menú con opciones mostrado por prompt.
+let menu = 
+`Por favor, ingrese una opción.
+1. ${bitcoin.nombreFormateado()}
+2. ${ethereum.nombreFormateado()}
+3. ${cardano.nombreFormateado()}
+4. Finalizar carga`
 
-const elegirOpcion = () => prompt("Por favor, ingrese una opción.\n1. BTC - Bitcoin\n2. ETH - Ethereum\n3. ADA - Cardano\n4. Finalizar carga")
+/** Funciones que aplican lógica de negocio */
 
+// Permite elegir una opción del menú y la retorna.
+const elegirOpcion = () => prompt(menu)
+
+// Imprime en el DOM el saldo en pesos y dólares del usuario según las transacciones ingresadas.
 const imprimirSaldos = () => {
     if(transacciones === 0){
         document.write(`<h4>No se han cargado transacciones.</h4>`)
@@ -35,34 +72,36 @@ const imprimirSaldos = () => {
                     </div>`)
 }
 
-const ingresarMonto = moneda => {
-    let monto = prompt(`Ingrese el monto en ${moneda}:`)
+// Ingresa por prompt la cantidad de unidades de la moneda en una transacción.
+const ingresarMonto = criptomoneda => {
+    let monto = prompt(`Ingrese el monto en ${criptomoneda.nombreFormateado()}:`)
+    // Se valida que sea correcto el monto ingresado.
     while(isNaN(monto) || monto <= 0){
-        monto = prompt(`El valor ingresado es incorrecto.\nPor favor, ingrese el monto en ${moneda}:`)
+        monto = prompt(`El valor ingresado es incorrecto.\nPor favor, ingrese el monto en ${criptomoneda.nombreFormateado()}:`)
     }
     return parseFloat(monto)
 }
 
-const imprimirTransaccion = (moneda, unidades, valorUSD) => {
-    document.write(`<div class="transaccion__container">
-                         <span class="transaccion">+ ${formatoCripto(unidades)} ${moneda} = ${formatoMoneda(valorUSD)} USD</span>
-                    </div>`)
+// Imprime en el DOM una transacción ingresada por el usuario, incluyendo la moneda y el monto en USD.
+const imprimirTransaccion = (transaccion) => {
+    document.write(
+        `<div class="transaccion__container">
+            <span class="transaccion">
+                + ${formatoCripto(transaccion.unidades)} ${transaccion.criptomoneda.sigla} = ${formatoMoneda(transaccion.montoEnUSD())} USD
+            </span>
+        </div>`)
     transacciones++;
 }
 
-/* DESAFIO COMPLEMENTARIO
-* El registro de una transacción se compone de 3 pasos.
-* 1. Ingresar el monto en unidades de la moneda elegida. 
-* 2. Calcular su valor monetario en USD.
-* 3. Mostrarla al usuario (imprimirla en el HTML).
-*/ 
-const registrarTransaccion = (moneda, precio) => {
-    let monto = ingresarMonto(moneda)
-    let valorUSD = calcularSaldo(precio, monto)
-    imprimirTransaccion(moneda, monto, valorUSD)
-    return valorUSD
+// Carga de una transacción. El usuario ingresa un monto, se crea una transacción, se la imprime y se retorna su monto en USD.
+const registrarTransaccion = (criptomoneda) => {
+    let unidades = ingresarMonto(criptomoneda)
+    let transaccion = new Transaccion(criptomoneda, unidades)
+    imprimirTransaccion(transaccion)
+    return transaccion.montoEnUSD()
 }
 
+// Carga de todas las transacciones vía prompt. Finaliza cuando el usuario elige la opción 4.
 const cargarTransacciones = () => {
     alert("Registro de transacciones en criptomonedas.")
     document.write("<h3>TRANSACCIONES:</h3>")
@@ -70,13 +109,13 @@ const cargarTransacciones = () => {
     while(opcion !== "4"){
         switch(opcion){
             case "1":
-                saldoUSD += registrarTransaccion("BTC", PRECIO_BITCOIN)
+                saldoUSD += registrarTransaccion(bitcoin)
                 break
             case "2":
-                saldoUSD += registrarTransaccion("ETH", PRECIO_ETHEREUM)
+                saldoUSD += registrarTransaccion(ethereum)
                 break
             case "3":
-                saldoUSD += registrarTransaccion("ADA", PRECIO_CARDANO)
+                saldoUSD += registrarTransaccion(cardano)
                 break
             default:
                 alert("ERROR: Opción no encontrada.")
@@ -89,6 +128,5 @@ const cargarTransacciones = () => {
 // Simulo el registro de transacciones y muestro balance cuando el usuario termina la carga.
 let saldoUSD = 0;
 let transacciones = 0;
-
 cargarTransacciones()
 imprimirSaldos()

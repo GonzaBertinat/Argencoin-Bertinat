8056a894920ae8de31a781ac138dd421e13a4fb9
Gonzalo Bertinat <gonzalo_bertinat_98@hotmail.com>
2021-10-22 21:24:31 -0300

    Desafío 4 - Simulador interactivo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,74 @@
-/* Desafío 3
- * Se solicita al usuario que ingrese una cantidad X números por prompt, y se calcula la sumatoria y el promedio de dichos valores. 
- * La cantidad X la elige el usuario.
- * Se valida que cada valor ingresado sea mayor a 0, de lo contrario, debe volver a ingresarlo.
+/* Desafío 4
+ * El proyecto consistirá en una plataforma de criptomonedas.
+ * Contará con la posibilidad de ver cotizaciones en vivo, cargar transacciones, 
+ * y ver el balance de activos. 
+ * Para este desafío simularé la carga de activos del usuario.
  */
 
-alert("¡Bienvenido! Se calculará la sumatoria y el promedio de los números que ingreses a continuación.")
+/* Cotizaciones en USD de las principales criptomonedas.
+   A futuro, se pedirán vía API */
+const BITCOIN = 60000
+const ETHEREUM = 3600
+const CARDANO = 2.20
 
-let cantidadValores = parseInt(prompt("Por favor, indique cuantos valores quiere ingresar."))
-if(cantidadValores > 0){
+// Precio DOLAR en Pesos Argentinos
+const COTIZACION_USD = 185
 
-    let suma = 0;
-    for(let i=0; i<cantidadValores; i++){
+const convertirAPesos = (dolares) => (dolares * COTIZACION_USD).toFixed(2)
 
-        let valor = parseInt(prompt("Por favor, ingrese un número mayor a 0."))
-        // Si ingresa un valor incorrecto, deberá volver a intentarlo hasta que sí lo sea.
-        while(valor <= 0){
-            valor = parseInt(prompt("El número ingresado es incorrecto. Por favor, ingrese un valor mayor a 0."))
-        }
-        suma += valor
+const calcularSaldo = (precio, unidades) => precio * unidades
+
+const elegirOpcion = () => prompt("Por favor, ingrese una opción.\n1. BTC - Bitcoin\n2. ETH - Ethereum\n3. ADA - Cardano\n4. Finalizar carga")
+
+const imprimirSaldos = () => {
+    if(transacciones === 0){
+        document.write(`<h4>No se han cargado transacciones.</h4>`)
     }
+    document.write(`<h3>SALDOS BILLETERA:</h3>
+                    <div class="saldos">
+                        <span class="pesos">Pesos (ARS): $${convertirAPesos(saldoUSD)}</span>
+                        <span class="dolares">Dólares (USD): $${saldoUSD.toFixed(2)}</span>
+                    </div>`)
+}
 
-    // Calculo el promedio con 4 decimales
-    let promedio = (suma / cantidadValores).toFixed(4) 
-    alert("La sumatoria de los valores ingresados es: " + suma)
-    alert("El promedio de los valores ingresados es: " + promedio)
+const registrarTransaccion = (moneda, precio) => {
+    let unidades = prompt(`Ingrese el monto en ${moneda}:`)
+    while(isNaN(unidades) || unidades <= 0){
+        unidades = prompt(`El valor ingresado es incorrecto.\nPor favor, ingrese el monto en ${moneda}:`)
+    }
+    document.write(`<div class="transaccion__container">
+                         <span class="transaccion">+ ${parseFloat(unidades).toFixed(6)} ${moneda}</span>
+                    </div>`)
+    transacciones++;
+    return calcularSaldo(precio,unidades)
 }
-else {
-    alert("La cantidad de valores debe ser mayor a cero. Por favor, recargue el sitio e inténtelo de nuevo.")
+
+const cargarTransacciones = () => {
+    alert("Registro de transacciones en criptomonedas.")
+    document.write("<h3>TRANSACCIONES:</h3>")
+    let opcion = elegirOpcion()
+    while(opcion !== "4"){
+        switch(opcion){
+            case "1":
+                saldoUSD += registrarTransaccion("BTC", BITCOIN)
+                break
+            case "2":
+                saldoUSD += registrarTransaccion("ETH", ETHEREUM)
+                break
+            case "3":
+                saldoUSD += registrarTransaccion("ADA", CARDANO)
+                break
+            default:
+                alert("ERROR: Opción no encontrada.")
+                break
+        }
+        opcion = elegirOpcion()
+    }    
 }
+
+// Simulo el registro de transacciones y muestro balance cuando el usuario termina la carga.
+let saldoUSD = 0;
+let transacciones = 0;
+
+cargarTransacciones()
+imprimirSaldos()
